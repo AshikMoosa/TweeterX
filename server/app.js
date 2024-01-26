1a03bd79e9fff6bb88ee4f3cc46c9f0c7d498bed
aka_Ashik <aka@rocketsoftware.com>
2024-01-26 18:04:19 +0530

    Added basic router

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const router = require("./router");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-  res.render("home-guest");
-});
+// Render Home-Guest Page
+app.use("/", router);
 
 app.listen(3000);
