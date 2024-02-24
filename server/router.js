8cadf6ff2e0ea87dc05ceb987b24b145ecbc4ccf
ashikmoosa <ashikmoosa96@gmail.com>
2024-02-24 20:39:27 +0530

    Added single page post

--- server/router.js
+++ server/router.js
@@ -11,5 +11,6 @@ router.post("/logout", userController.logout);
 
 // post related routes
 router.post("/create-post", userController.mustBeLoggedIn, postController.create);
+router.get("/post/:id", postController.viewSingle)
 
 module.exports = router;
